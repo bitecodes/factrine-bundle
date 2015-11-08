40e2fc8cec2dd9d3f181838c896f17cde73d7259
Thomas Uhlmann <tho.uhlmann@gmail.com>
2015-11-08 17:14:33 +0100

    Put yaml files in default directory

--- Command/DoctrineGenerateFactoryFileCommand.php
+++ Command/DoctrineGenerateFactoryFileCommand.php
@@ -5,6 +5,7 @@ namespace Fludio\DoctrineEntityFactoryBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
 
 class DoctrineGenerateFactoryFileCommand extends ContainerAwareCommand
 {
@@ -17,13 +18,40 @@ class DoctrineGenerateFactoryFileCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $parser = new Parser();
         $dumper = $this->getContainer()->get('yaml_dumper');
 
+        $entityConfigs = $this->getContainer()->get('fludio_factory.schema_reader')->read();
 
-        $array = $this->getContainer()->get('fludio_factory.schema_reader')->read();
+        $kernel = $this->getContainer()->get('kernel');
+        $bundles = $kernel->getBundles();
 
-        $yaml = $dumper->dump($array, 100);
+        foreach($bundles as $bundle) {
+            $factoryDir = $bundle->getPath() . '/Resources/config/entity-factory';
+            $bundleNamespace = $bundle->getNamespace();
 
-        file_put_contents('./entites.yml', $yaml);
+            foreach($entityConfigs as $entity => $config) {
+                $refl = new \ReflectionClass($entity);
+                if(strpos($refl->getNamespaceName(), $bundleNamespace) !== false) {
+                    $path = $factoryDir . DIRECTORY_SEPARATOR . $refl->getShortName() . '.yml';
+
+                    if(!file_exists($factoryDir)) {
+                        mkdir($factoryDir, 0777, true);
+                    }
+
+                    if(file_exists($path)) {
+                        $oldYaml = file_get_contents($path);
+                        $oldConfig = $parser->parse($oldYaml);
+                        if($oldConfig) {
+                            $config = array_replace_recursive($config, $oldConfig[$entity]);
+                        }
+                    }
+
+                    $yaml = $dumper->dump([$entity => $config], 100);
+
+                    file_put_contents($path, $yaml);
+                }
+            }
+        }
     }
 }
