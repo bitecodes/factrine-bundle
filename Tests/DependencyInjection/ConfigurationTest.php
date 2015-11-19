832f8ca856fb2aed1b29efc37d293d07be0a123c
Thomas Uhlmann <tho.uhlmann@gmail.com>
2015-11-19 09:23:34 +0100

    Fix services.yml file

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Fludio\DoctrineEntityFactoryBundle\Tests\DependencyInjection;
+
+
+use Symfony\Component\Yaml\Parser;
+
+class Configuration extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function services_file_is_formatted_correctly()
+    {
+        $yaml = new Parser();
+        $servicesYaml = file_get_contents(__DIR__ . './../../Resources/config/services.yml');
+        $services = $yaml->parse($servicesYaml, true);
+
+        $this->assertTrue(is_array($services));
+    }
+}
