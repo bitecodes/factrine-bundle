868ba76b2a6d2dda8c9d800fc3593e2cf3e1e49a
Thomas Uhlmann <tho.uhlmann@gmail.com>
2015-11-22 18:29:27 +0100

    Rename Extension file

--- DependencyInjection/FludioFactrineExtension.php
+++ DependencyInjection/FludioFactrineExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class FludioFactrineBundleExtension extends Extension
+class FludioFactrineExtension extends Extension
 {
     /**
      * {@inheritdoc}
